a05657576e9d3dd4edf483394f4fc76a0f69782e
Colin Zeidler <c.zeidler.wc@gmail.com>
2014-10-28 22:39:20 -0400

    testing Swing UI

--- src/dcm3203/Controller.java
+++ src/dcm3203/Controller.java
@@ -10,5 +10,7 @@ public class Controller {
      */
     public static void main(String[] args) {
         System.out.println("Hello!");
+        View myView = new View(new Model());
+        System.out.println("Goodby");
     }
 }
--- src/dcm3203/View.java
+++ src/dcm3203/View.java
@@ -1,7 +1,20 @@
 package dcm3203;
 
+import javax.swing.*;
+import java.awt.*;
+
 /**
  * Created by Colin on 2014-10-28.
  */
-public class View {
+public class View extends JFrame{
+    private Model myModel;
+
+    public View(Model model) {
+        setTitle("Application name");
+        setSize(500, 600);
+        setLocation(100, 100);
+
+        setLayout(new GridLayout(2, 3));
+        setVisible(true);
+    }
 }
