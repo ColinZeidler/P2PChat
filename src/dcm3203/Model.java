d2076385c99240d3df09a6ca968ce0b8ad41447e
Colin Zeidler <c.zeidler.wc@gmail.com>
2014-10-28 22:02:22 -0400

    added main method to Controller, as it is the entry point for the program
    added Java gitignore file

--- src/dcm3203/Model.java
+++ src/dcm3203/Model.java
@@ -1,7 +1,23 @@
 package dcm3203;
 
+import java.util.ArrayDeque;
+
 /**
  * Created by Colin on 2014-10-28.
  */
 public class Model {
+    private ArrayDeque<String> messageHistory;
+
+    public ArrayDeque<String> getMessageHistory() {
+        return messageHistory;
+    }
+
+    public void setMessageHistory(ArrayDeque<String> messageHistory) {
+        this.messageHistory = messageHistory;
+    }
+
+    public void addMessage(String message) {
+        messageHistory.add(message);
+    }
+
 }
