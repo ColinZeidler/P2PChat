e17252a138138a40b3fe4d8ae79d1e4eed047c8b
Colin Zeidler <c.zeidler.wc@gmail.com>
2014-10-28 21:53:09 -0400

    added main method to Controller, as it is the entry point for the program
    added Java gitignore file

--- src/dcm3203/Controller.java
+++ src/dcm3203/Controller.java
@@ -4,5 +4,11 @@ package dcm3203;
  * Created by Colin on 2014-10-28.
  */
 public class Controller {
-
+    /**
+     * Entry method
+     * @param args
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello!");
+    }
 }
