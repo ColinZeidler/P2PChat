ed23dd536d3fa923f3f0becffca53a2a2a191b2e
Michael van Dyk <msvandyk@hotmail.com>
2014-10-31 14:51:06 -0400

    Made the components general to the height and width set for changing the dialog's size easier, all that needs to be done is getting the local addressed to display, sending the selected IP to the main part of the program and exiting properly

--- src/dcm3203/ConnectDialog.java
+++ src/dcm3203/ConnectDialog.java
@@ -2,10 +2,7 @@ package dcm3203;
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.event.MouseAdapter;
-import java.awt.event.MouseEvent;
+import java.awt.event.*;
 
 /**
  * Created by Michael on 30/10/2014.
@@ -13,25 +10,52 @@ import java.awt.event.MouseEvent;
  *
  * NOTES:
  *  - IP assumed to be in strings
+ *  TODO call correct function to send specified IP to main part of program
+ *  TODO make sure exit button works with main program
+ *  TODO get local IPs to list
  *
  */
 public class ConnectDialog extends JDialog{
 
+    /////
+    //   All the components
+    //
+    private JButton             joinListedIP;
+    private JButton             joinEnteredIP;
+    private JButton             cancelButton;
     private JList<String>       listIP;
+    private JScrollPane         listPane;
     private JTextField          enterIP;
 
+    /////
+    //   The handlers
+    //
     private ActionListener      cancelAction;       //  The handler for quitting the app
     private ActionListener      listIPAction;       //  The handler for an IP found by discovery
     private ActionListener      enterIPAction;      //  The handler for an IP entered by user
+    private ComponentAdapter    resizeAdapter;      //  TODO get this working
     private MouseAdapter        listIPAdapter;      //  The handle for clicking on the list
 
-    static final String         NO_PEERS = "No peers found";        //  Empty list const string
+    /////
+    //   The constant values
+    //
+    static private final boolean        CDIALOG_RESIZABLE = false; // 'true' doesn't work properly
+
+    static private final int            CDIALOG_WIDTH = 600;                //  Window width
+    static private final int            CDIALOG_HEIGHT = 400;               //  Window height
+    static private final int            CDIALOG_PAD = 5;                    //  Padding between components
+
+    static private final int            CDIALOG_BUTTON_WIDTH = 150;         //  Width of each button
+    static private final int            CDIALOG_BUTTON_HEIGHT = 25;         //  Height of each button (and textbox)
+
+    static private final String         NO_PEERS = "No peers found";        //  Empty list const string
 
     public ConnectDialog(Frame owner, String title, boolean modal) {
         super(owner, title, modal);
 
         initHandlers();
         initVisual();
+        updatePositions();
         initList();
     }
 
@@ -61,6 +85,13 @@ public class ConnectDialog extends JDialog{
             }
         };
 
+        resizeAdapter = new ComponentAdapter() {
+            @Override
+            public void componentResized(ComponentEvent e) {
+                updatePositions();
+            }
+        };
+
         listIPAdapter = new MouseAdapter() {
             @Override
             public void mouseClicked(MouseEvent e) {
@@ -74,46 +105,75 @@ public class ConnectDialog extends JDialog{
     //   This function deals with setting up the visuals of the dialog
     //
     private void initVisual() {
-        setSize(500, 390);
-        setResizable(false);
+        setResizable(CDIALOG_RESIZABLE);
+
+        if (isResizable()) {
+            setSize(CDIALOG_WIDTH + CDIALOG_PAD * 2, CDIALOG_HEIGHT + CDIALOG_PAD * 2);
+        }  else {
+            setSize(CDIALOG_WIDTH, CDIALOG_HEIGHT);
+        }
+
+        addComponentListener(resizeAdapter);
+
+        setLocation((int)(getToolkit().getScreenSize().getWidth()/2 - getWidth()/2),
+                (int)(getToolkit().getScreenSize().getHeight()/2 - getHeight()/2));
         setLayout(null);
 
         listIP = new JList<String>();
         //listIP.addMouseListener(listListener);
-        JScrollPane scrollPane = new JScrollPane(listIP,
+        listPane = new JScrollPane(listIP,
                 ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS,
                 ScrollPaneConstants.HORIZONTAL_SCROLLBAR_AS_NEEDED);
-        scrollPane.setLocation(5,5);
-        scrollPane.setSize(330, 280);
+        listPane.setLocation(CDIALOG_PAD, CDIALOG_PAD);
         listIP.addMouseListener(listIPAdapter);
-        this.add(scrollPane);
+        this.add(listPane);
 
         enterIP = new JTextField("[Enter an IP Address]");
-        enterIP.setLocation(5,300);
-        enterIP.setSize(330,25);
         this.add(enterIP);
 
         /////
         //   Sets up the buttons
         //
 
-        JButton joinListedIP = new JButton("Join Listed IP");
-        joinListedIP.setLocation(340,30);
-        joinListedIP.setSize(150, 25);
+        joinListedIP = new JButton("Join Listed IP");
         joinListedIP.addActionListener(listIPAction);
         add(joinListedIP);
 
-        JButton joinEnteredIP = new JButton("Join Entered IP");
-        joinEnteredIP.setLocation(340,300);
-        joinEnteredIP.setSize(150,25);
+        joinEnteredIP = new JButton("Join Entered IP");
         joinEnteredIP.addActionListener(enterIPAction);
         add(joinEnteredIP);
 
-        JButton cancelButton = new JButton("Exit");
-        cancelButton.setLocation(340,330);
-        cancelButton.setSize(150, 25);
+        cancelButton = new JButton("Exit");
         cancelButton.addActionListener(cancelAction);
         add(cancelButton);
+    }
+
+    /////
+    //   This function sets up the positions and width/heights of the components
+    //
+    private void updatePositions() {
+        listPane.setSize(CDIALOG_WIDTH - CDIALOG_BUTTON_WIDTH - CDIALOG_PAD*4,
+                CDIALOG_HEIGHT - (CDIALOG_PAD*5 + CDIALOG_BUTTON_HEIGHT*3));
+
+        enterIP.setLocation(CDIALOG_PAD,
+                CDIALOG_HEIGHT - (CDIALOG_PAD + CDIALOG_BUTTON_HEIGHT)*3);
+        enterIP.setSize(listPane.getWidth(),
+                CDIALOG_BUTTON_HEIGHT);
+
+        joinListedIP.setLocation(CDIALOG_WIDTH - CDIALOG_BUTTON_WIDTH - CDIALOG_PAD*2,
+                CDIALOG_PAD + CDIALOG_BUTTON_HEIGHT);
+        joinListedIP.setSize(CDIALOG_BUTTON_WIDTH,
+                CDIALOG_BUTTON_HEIGHT);
+
+        joinEnteredIP.setLocation(joinListedIP.getLocation().x,
+                CDIALOG_HEIGHT - (CDIALOG_PAD + CDIALOG_BUTTON_HEIGHT)*3);
+        joinEnteredIP.setSize(CDIALOG_BUTTON_WIDTH,
+                CDIALOG_BUTTON_HEIGHT);
+
+        cancelButton.setLocation(joinListedIP.getLocation().x,
+                CDIALOG_HEIGHT - (CDIALOG_PAD + CDIALOG_BUTTON_HEIGHT)*2);
+        cancelButton.setSize(CDIALOG_BUTTON_WIDTH,
+                CDIALOG_BUTTON_HEIGHT);
     }
 
     private void initList() {
@@ -194,9 +254,11 @@ public class ConnectDialog extends JDialog{
         return(true);
     }
 
+    /*
 // Testing stuff
     static public void main(String[] args){
         ConnectDialog dlog = new ConnectDialog(new View(new Model(), new Controller()),"Connect",true);
         dlog.setVisible(true);
     }
+    */
 }
