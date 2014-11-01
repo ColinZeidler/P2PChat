0002d4d419828401928a77f22a74f73a9c463e7e
Colin Zeidler <c.zeidler.wc@gmail.com>
2014-11-01 18:15:18 -0400

    code for remote end of network discovery

--- src/dcm3203/network/UDPDiscoveryHandle.java
+++ src/dcm3203/network/UDPDiscoveryHandle.java
@@ -1,5 +1,8 @@
 package dcm3203.network;
 
+import java.io.IOException;
+import java.net.*;
+
 /**
  * Created by Michael on 31/10/2014.
  *
@@ -7,6 +10,9 @@ package dcm3203.network;
  */
 public class UDPDiscoveryHandle implements Runnable {
     private int port;
+    private DatagramSocket socket;
+    private static final String discoverRequest = "p2p_chat_discovery_packet";
+    private static final String discoverResponse = "p2p_chat_discovery_response";
 
     public UDPDiscoveryHandle(int port) {
         this.port = port;
@@ -18,10 +24,33 @@ public class UDPDiscoveryHandle implements Runnable {
      * checks if the message is the discovery message
      * if yes, reply with reply message (and user count)
      */
+    @Override
     public void run(){
-        // TODO add code to handle discovery
-        System.out.println("temp out, nothing actually done");
         System.out.println("Port is: " + this.port);
+        try {
+            socket = new DatagramSocket(port, InetAddress.getByName("0.0.0.0"));
+            socket.setBroadcast(true);
+
+            while (true) {
+                //receive packet
+                byte[] recvBuff = new byte[4096];
+                DatagramPacket packet = new DatagramPacket(recvBuff, recvBuff.length);
+                socket.receive(packet);
+
+                //get data from packet
+                String message = new String(packet.getData()).trim();
+                if (message.equals(discoverRequest)) {
+                    byte[] sendData = discoverResponse.getBytes();
+
+                    //send response to origin
+                    DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, packet.getAddress(), packet.getPort());
+                    socket.send(sendPacket);
+                }
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
 }
