107dc387f80d70624d4ba63e63231329e58ca2c3
Michael van Dyk <msvandyk@hotmail.com>
2014-11-12 19:35:24 -0500

    Commented FileData

--- src/dcm3203/data/FileData.java
+++ src/dcm3203/data/FileData.java
@@ -14,6 +14,23 @@ import java.io.File;
  *
  *     Only create a FileData object if you have the file OR you have a FileData string
  *
+ *  Use:
+ *      Check validity before use
+ *          Before use check if valid with - boolean isValid()
+ *
+ *      To get the FileData String for sending
+ *          If sending the file information to another peer use - String getSendDataString()
+ *          If storing locally use - String getDataString()
+ *
+ *      To create a FileData object with a FileData String:
+ *          Constructor : FileData(String) where the string is in FileData format
+ *          Function : boolean setFromDataString(String), returns false if not valid FileData
+ *
+ *      FileData stores 4 pieces of info on the file:
+ *          name - the name of the file (e.g. info.txt)
+ *          location - the directory path (if local) or the peer name (if not local)
+ *          fileSize - the size of the file
+ *          have - if true the file is local
  */
 public class FileData {
 
@@ -57,6 +74,9 @@ public class FileData {
         return (name + SPLIT_STR + location + SPLIT_STR + fileSize + SPLIT_STR + have);
     }
 
+    /////
+    //   Basic getter functions
+    //
     public String getFileLocation() { return (location); }
     public String getFileName()     { return (name); }
     public long   getFileSize()     { return (fileSize); }
@@ -74,6 +94,9 @@ public class FileData {
         }
     }
 
+    /////
+    //   Returns a boolean of whether the current users has the file or not
+    //
     public boolean isHave() { return (have); }
 
     /////
@@ -82,9 +105,9 @@ public class FileData {
     public boolean isValid() { return (valid); }
 
     /////
-    //   Gets data from a FileData string
+    //   Gets data from a FileData string, returns if the object is valid
     //
-    public void setFromDataString(String dataString) {
+    public boolean setFromDataString(String dataString) {
         String[] split = dataString.split(SPLIT_STR);
 
         try {
@@ -100,6 +123,7 @@ public class FileData {
             valid = false;
             System.err.println(e.getMessage());
         }
+        return (valid);
     }
 
 }
