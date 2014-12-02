d5c500af1cc9d7dc074219893e502dc0b1b340fd
Michael van Dyk <msvandyk@hotmail.com>
2014-12-02 17:15:02 -0500

    GetFileDialog is finished

--- src/dcm3203/ui/GetFileDialog.java
+++ src/dcm3203/ui/GetFileDialog.java
@@ -3,6 +3,8 @@ package dcm3203.ui;
 import dcm3203.data.FileData;
 
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.Collections;
 import java.util.LinkedList;
 import java.util.List;
@@ -20,9 +22,11 @@ public class GetFileDialog extends JDialog {
     private final JList<String> fileJList;
     private final JScrollPane listPane;
 
-    private static final String[] NO_FILES = {"You have advertised no files"};
+    private FileData fileSelected;
 
-    public GetFileDialog(Frame owner, String title, boolean modal, List<FileData> fileList) {
+    private static final String[] NO_FILES = {"No Files Advertised"};
+
+    public GetFileDialog(Frame owner, String title, boolean modal, final List<FileData> fileList) {
         super(owner, title, modal);
         this.fileList = fileList;
 
@@ -40,6 +44,31 @@ public class GetFileDialog extends JDialog {
         listPane.setSize(485,300);
         this.add(listPane);
 
+        JButton cancelButton = new JButton("Cancel");
+        cancelButton.setLocation(340, listPane.getHeight() + 10);
+        cancelButton.setSize(150, 25);
+        cancelButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                fileSelected = null;
+                setVisible(false);
+            }
+        });
+        add(cancelButton);
+
+        JButton selectFileButton = new JButton("Select");
+        selectFileButton.setSize(cancelButton.getSize());
+        selectFileButton.setLocation(cancelButton.getLocation().x - selectFileButton.getWidth() - 5,
+                cancelButton.getLocation().y);
+        selectFileButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                fileSelected = fileList.get(fileJList.getSelectedIndex());
+                setVisible(false);
+            }
+        });
+        add(selectFileButton);
+
         if (fileList.isEmpty()) {
             fileJList.setListData(NO_FILES);
         } else {
@@ -56,4 +85,5 @@ public class GetFileDialog extends JDialog {
         this.setVisible(true);
     }
 
+    public FileData getFileSelected() { return (fileSelected); }
 }
