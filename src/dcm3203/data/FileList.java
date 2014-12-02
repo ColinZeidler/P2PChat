a2680331ed23d7934c3bae941377716311a3149b
Michael van Dyk <msvandyk@hotmail.com>
2014-12-02 07:04:16 -0500

    Updated to use LinkedLists for easy sorting on insertion, added a few functions updated name etc.

--- src/dcm3203/data/FileList.java
+++ src/dcm3203/data/FileList.java
@@ -1,7 +1,10 @@
 package dcm3203.data;
 
-import java.util.Vector;
+import java.util.List;
+import java.util.LinkedList;
+import java.util.Map;
 import java.util.HashMap;
+import java.util.ListIterator;
 
 /**
  * Created by Michael on 12/11/2014.
@@ -17,6 +20,15 @@ import java.util.HashMap;
  *          - If local (on the current computer) use - void addToMyList(FileData)
  *          - If from an advertisement (from other peer) use - void add(User, FileData)
  *
+ *      To see if a list contains a file
+ *          - If local use - boolean isAdvertisedByMe(FileData)
+ *          - If another peer - boolean isAdvertisedByUser(User, FileData)
+ *
+ *      To get files:
+ *          - Local - Entire list - List<FileData> getLocalFiles()
+ *          - Users - Entire map - Map<User, List<FileData>> getUserFiles()
+ *                  - User's list - List<FileData> getUserFiles(User)
+ *
  *      To remove a file:
  *          - If local use - boolean removeFromMyList(FileData)
  *          - If from another peer use - boolean remove(FileData)
@@ -27,58 +39,92 @@ import java.util.HashMap;
  *
  */
 public class FileList {
-    private HashMap<User, Vector<FileData>> fileList;   //  The list of files that the other users are advertising
-    private Vector<FileData> myFiles;                   //  The files that the current user is advertising
+    private Map<User, List<FileData>> userFiles;   //  The list of files that the other users are advertising
+    private List<FileData> myFiles;                   //  The files that the current user is advertising
 
     FileList() {
-        fileList = new HashMap<User, Vector<FileData>>();
-        myFiles = new Vector<FileData>();
+        userFiles = new HashMap<User, List<FileData>>();
+        myFiles = new LinkedList<FileData>();
     }
 
     /////
     //   Adds a file to the list of files that the specified user is advertising
     //
     public synchronized void add(User user, FileData fileData) {
-        Vector<FileData> fdv;
-        if (!fileList.containsKey(user)) {
-            fdv = new Vector<FileData>();
-            fdv.add(fileData);
-            fileList.put(user, fdv);
+        List<FileData> fdv;
+
+        if (!userFiles.containsKey(user)) {
+            fdv = new LinkedList<FileData>();
+            userFiles.put(user, fdv);
         } else {
-            fdv = fileList.get(user);
-            fdv.add(fileData);
-            fileList.put(user, fdv);
+            fdv = userFiles.get(user);
         }
+
+        insertToList(fdv, fileData);
     }
 
     /////
     //   Adds a file to the list of files the local user is advertising
     //
-    public synchronized void addToMyList(FileData fileData) {
-        myFiles.add(fileData);
-    }
+    public synchronized void addToMyList(FileData fileData) { insertToList(myFiles, fileData); }
 
     /////
     //   Clears the list of files the local user is advertising
     //
-    public synchronized void clearMyList() {
-        myFiles.clear();
+    public synchronized void clearMyList() { myFiles.clear(); }
+
+    /////
+    //   Gives a list containing the local files
+    //
+    public synchronized List<FileData> getLocalFiles() { return (myFiles); }
+
+    /////
+    //   Gives a map containing the names of local files
+    //
+    public synchronized Map<User, List<FileData>> getUserFiles() { return (userFiles); }
+
+    /////
+    //   Gives a list of all files from a specified user
+    //
+    public synchronized List<FileData> getUserFiles(User user) { return (userFiles.get(user)); }
+
+    /////
+    //   This function inserts a FileData and inserts lexicographically to prevent sorting later
+    //
+    private synchronized void insertToList(List<FileData> list, FileData fileData) {
+        if (!list.isEmpty()) {
+            ListIterator<FileData> iterator = list.listIterator();
+
+            while (iterator.hasNext()) {
+                if (iterator.next().compareTo(fileData) > 0) {
+                    iterator.previous();        //  Needs to go back one since when getting
+                    iterator.add(fileData);     // the data the iterator goes forward
+
+                    return;
+                }
+            }
+        }
+        list.add(fileData);
     }
+
+    public synchronized boolean isAdvertisedByMe(FileData fileData) { return (myFiles.contains(fileData)); }
+
+    public synchronized boolean isAdvertisedByUser(User user, FileData fileData) { return (userFiles.get(user).contains(fileData)); }
 
     /////
     //   Removes a single file from the list (if add functionality to remove a file that a user does
     //  not want to advertise anymore)
     //
     public synchronized boolean remove(FileData fileData) {
-        for (User user : fileList.keySet()) {
-            Vector<FileData> fdv = fileList.get(user);
+        for (User user : userFiles.keySet()) {
+            List<FileData> fdv = userFiles.get(user);
 
             if (fdv.contains(fileData)) {
                 fdv.remove(fileData);
                 if (fdv.isEmpty()) {
-                    fileList.remove(user);
+                    userFiles.remove(user);
                 } else {
-                    fileList.put(user, fdv);
+                    userFiles.put(user, fdv);
                 }
                 return (true);
             }
@@ -90,8 +136,8 @@ public class FileList {
     //   Removes all the files that are advertised by the user that is removed
     //
     public synchronized boolean remove(User user){
-        if (fileList.containsKey(user)) {
-            fileList.remove(user);
+        if (userFiles.containsKey(user)) {
+            userFiles.remove(user);
             return (true);
         }
         return (false);
@@ -109,11 +155,6 @@ public class FileList {
     }
 
     /////
-    //   Gives an vector containing the names of local files
-    //
-    public synchronized Vector<FileData> localFiles() { return (myFiles); }
-
-    /////
     //   Returns a text list of the files advertised, both that the user is advertising and that
     //  peers are advertising
     //
@@ -125,9 +166,9 @@ public class FileList {
         }
         strList += "\n";
 
-        for (User user : fileList.keySet()) {
+        for (User user : userFiles.keySet()) {
             strList += user.getName() + ": ";
-            for (FileData fileData : fileList.get(user)) {
+            for (FileData fileData : userFiles.get(user)) {
                 strList += fileData.getFileName() + ", ";
             }
             strList += "\n";
